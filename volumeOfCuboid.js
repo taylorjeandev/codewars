fa75107b6251f22a60fff9e3e14885679b1cea59
Taylor Jean <taylorgjean@gmail.com>
2022-05-30 06:18:28 -0500

    volumeOfCuboid.js

---
+++ volumeOfCuboid.js
@@ -0,0 +1,11 @@
+// Bob needs a fast way to calculate the volume of a cuboid with three values: the length, width and height of the cuboid. Write a function to help Bob with this calculation.
+
+class Kata {
+    static getVolumeOfCuboid(length, width, height) {
+      return length * width * height;
+    }
+  }
+
+
+// Test.assertEquals(Kata.getVolumeOfCuboid(1,2,2), 4);
+// Test.assertEquals(Kata.getVolumeOfCuboid(6.3,2,5), 63);
