f51a153fff7d7717118043e525a3bccaf802c8ba
Taylor Jean <taylorgjean@gmail.com>
2022-05-31 13:09:56 +0100

    cantSleepCountSheep.js

---
+++ cantSleepCountSheep.js
@@ -0,0 +1,17 @@
+// If you can't sleep, just count sheep!!
+
+// Task:
+// Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
+
+let countSheep = function (num){
+    let str = "";
+    for(let i = 1; i <= num; i++) {
+         str+= `${i} sheep...`; 
+        }
+    return str;
+  }
+
+//   assert.strictEqual(countSheep(0), "");
+//     assert.strictEqual(countSheep(1), "1 sheep...");
+//     assert.strictEqual(countSheep(2), "1 sheep...2 sheep...");
+//     assert.strictEqual(countSheep(3), "1 sheep...2 sheep...3 sheep...");
