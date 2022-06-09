8d7f3155402e5e1c57e5578868b9d20a3a8c75cf
Taylor Jean <taylorgjean@gmail.com>
2022-06-09 11:23:01 +0200

    l1-set-alarm.js

---
+++ l1-set-alarm.js
@@ -0,0 +1,22 @@
+// Write a function named setAlarm which receives two parameters. The first parameter, employed, is true whenever you are employed and the second parameter, vacation is true whenever you are on vacation.
+
+// The function should return true if you are employed and not on vacation (because these are the circumstances under which you need to set an alarm). It should return false otherwise. Examples:
+
+// setAlarm(true, true) -> false
+// setAlarm(false, true) -> false
+// setAlarm(false, false) -> false
+// setAlarm(true, false) -> true
+
+function setAlarm(employed, vacation){
+    if (vacation == true){
+        return false;
+    }else if (employed == false){
+        return false;
+    }else if (employed == true && vacation == false){
+        return true;
+    }
+}
+
+// assert.strictEqual(setAlarm(true, true),  false,"Should be false.");
+// assert.strictEqual(setAlarm(false,true),  false, "Should be false.");
+// assert.strictEqual(setAlarm(true, false), true,"Should be true.");
