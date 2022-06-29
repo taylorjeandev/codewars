8f7214a0469c8ce9d0c9ad1b65f053519cf7d5d3
Taylor Jean <taylorgjean@gmail.com>
2022-06-29 19:49:59 +0200

    well-of-ideas.js

--- well-of-ideas.js
+++ well-of-ideas.js
@@ -3,22 +3,17 @@
 // In this kata you need to check the provided array (x) for good ideas 'good' and bad ideas 'bad'. If there are one or two good ideas, return 'Publish!', if there are more than 2 return 'I smell a series!'. If there are no good ideas, as is often the case, return 'Fail!'.
 
 function well(x){
-    let good = 0
+    let good = 0;
 
- for(let i = 0; i <= x.length; i++){
+ for(let i = 0; i < x.length; i++){
     if (x[i] === 'good'){
-       good++;
+       good += 1;
     }
+    
 }
-
-    if (good > 0){
-        return 'Publish!'
-    } else if(good > 2){
-        return 'I smell a series!'
-    }else{
-        return 'Fail!'
-    }
-
+if (good <= 0) return 'Fail!'; 
+  
+  return good > 2 ? 'I smell a series!' : 'Publish!';
 }
 
 
