ccac09df489090399f5cc2e8c9ab1b9aeff1774e
Taylor Jean <taylorgjean@gmail.com>
2022-07-02 10:33:29 +0200

    filling-array-part-one.js

---
+++ filling-array-part-one.js
@@ -0,0 +1,20 @@
+// We want an array, but not just any old array, an array with contents!
+
+// Write a function that produces an array with the numbers 0 to N-1 in it.
+
+// For example, the following code will result in an array containing the numbers 0 to 4:
+
+// arr(5) // => [0,1,2,3,4]
+// Note: The parameter is optional. So you have to give it a default value.
+
+function arr (n) {
+    let array = [];
+    for(let i = 0; i < n; i++){
+        array.push(i);
+    }
+    return array;
+}
+
+// it('should return an array',_=>Test.expect(arr() instanceof Array));
+// it('should return a blank array when called with no argument',_=>Test.assertSimilar(arr(), []));
+// it('should return 0 to 3 when called with 4',_=>Test.assertSimilar(arr(4), [0,1,2,3]));
