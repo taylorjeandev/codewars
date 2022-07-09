ccee9e9292a81b8b6f9bf6a1a9b9e043f2de3f51
Taylor Jean <taylorgjean@gmail.com>
2022-07-09 09:15:58 +0200

    july 9th

---
+++ surface-area-and-volume-of-box.js
@@ -0,0 +1,7 @@
+// Write a function that returns the total surface area and volume of a box as an array: [area, volume]
+
+function getSize(width, height, depth){
+let array = [2 * (length*2 + height*2 + width*2), width * height * depth]
+    return array
+}
+
