95f099e41be6e81925578c5c101d78d879e3977c
Taylor Jean <taylorgjean@gmail.com>
2022-07-10 10:10:26 +0200

    july 10

---
+++ swap-values.js
@@ -0,0 +1,12 @@
+// I would like to be able to pass an array with two elements to my swapValues function to swap the values. However it appears that the values aren't changing.
+
+// Can you figure out what's wrong here?
+
+function swapValues(args) {
+    var temp = args[0];
+    args[0] = args[1];
+    args[1] = temp;
+}
+
+// Test.assertEquals(arr[0], 2, "Failed swapping numbers")
+// Test.assertEquals(arr[1], 1, "Failed swapping numbers")
