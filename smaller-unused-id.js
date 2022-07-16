f1494a46ccd18a99e2e0b18207f6f17f59a58397
Taylor Jean <taylorgjean@gmail.com>
2022-07-16 08:53:38 +0200

    july 16th

---
+++ smaller-unused-id.js
@@ -0,0 +1,19 @@
+// Hey awesome programmer!
+
+// You've got much data to manage and of course you use zero-based and non-negative ID's to make each data item unique!
+
+// Therefore you need a method, which returns the smallest unused ID for your next new data item...
+
+// Note: The given array of used IDs may be unsorted. For test reasons there may be duplicate IDs, but you don't have to find or remove them!
+
+// Go on and code some pure awesomeness!
+
+function nextId(ids){
+    let x = 0;
+    while (ids.includes(x)) x++;
+    return x;
+  }
+// Test.assertEquals(nextId([0,1,2,3,5]), 4);
+// });
+// it('returns the lowestNextId', function() {
+//   Test.assertEquals(nextId([0,1,2,3,4,5,6,7,8,9,10]), 11);
