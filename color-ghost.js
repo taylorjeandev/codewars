4383c5266064ff856d7ab77de2bb9ff585c2aa05
Taylor Jean <taylorgjean@gmail.com>
2022-07-17 10:56:07 +0200

    july 17th

---
+++ color-ghost.js
@@ -0,0 +1,14 @@
+// Create a class Ghost
+
+// Ghost objects are instantiated without any arguments.
+
+// Ghost objects are given a random color attribute of "white" or "yellow" or "purple" or "red" when instantiated
+
+// ghost = new Ghost();
+// ghost.color //=> "white" or "yellow" or "purple" or "red"
+
+let Ghost = function(){
+    let ghostColor = ['white', 'yellow', 'purple', 'red']
+    let ghostIndex = Math.floor(Math.random() * ghostColor.length);
+    this.color = ghostColor[ghostIndex]
+}
