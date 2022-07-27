73562c4e0762b32f863c6101b1761971d880f61e
Taylor Jean <taylorgjean@gmail.com>
2022-07-27 13:08:25 +0200

    july 27

---
+++ exclamation-mark-six.js
@@ -0,0 +1,32 @@
+// Remove n exclamation marks in the sentence from left to right. n is positive integer.
+
+// Examples
+// remove("Hi!",1) === "Hi"
+// remove("Hi!",100) === "Hi"
+// remove("Hi!!!",1) === "Hi!!"
+// remove("Hi!!!",100) === "Hi"
+// remove("!Hi",1) === "Hi"
+// remove("!Hi!",1) === "Hi!"
+// remove("!Hi!",100) === "Hi"
+// remove("!!!Hi !!hi!!! !hi",1) === "!!Hi !!hi!!! !hi"
+// remove("!!!Hi !!hi!!! !hi",3) === "Hi !!hi!!! !hi"
+// remove("!!!Hi !!hi!!! !hi",5) === "Hi hi!!! !hi"
+// remove("!!!Hi !!hi!!! !hi",100) === "Hi hi hi"
+
+function remove(s,n){
+    for(let i = 0; i < n; i++)
+      s = s.replace('!', "")
+    return s
+  }
+  
+// Test.assertSimilar(remove("Hi!",1) , "Hi")
+// Test.assertSimilar(remove("Hi!",100) , "Hi")
+// Test.assertSimilar(remove("Hi!!!",1) , "Hi!!")
+// Test.assertSimilar(remove("Hi!!!",100) , "Hi")
+// Test.assertSimilar(remove("!Hi",1) , "Hi")
+// Test.assertSimilar(remove("!Hi!",1) , "Hi!")
+// Test.assertSimilar(remove("!Hi!",100) , "Hi")
+// Test.assertSimilar(remove("!!!Hi !!hi!!! !hi",1) , "!!Hi !!hi!!! !hi")
+// Test.assertSimilar(remove("!!!Hi !!hi!!! !hi",3) , "Hi !!hi!!! !hi")
+// Test.assertSimilar(remove("!!!Hi !!hi!!! !hi",5) , "Hi hi!!! !hi")
+// Test.assertSimilar(remove("!!!Hi !!hi!!! !hi",100) , "Hi hi hi")
---
+++ fundamentals-return.js
@@ -0,0 +1,48 @@
+// Make multiple functions that will return the sum, difference, modulus, product, quotient, and the exponent respectively.
+
+// Please use the following function names:
+
+// addition = add
+
+// multiply = multiply
+
+// division = divide (both integer and float divisions are accepted)
+
+// modulus = mod
+
+// exponential = exponent
+
+// subtraction = subt
+
+// Note: All math operations will be: a (operation) b
+
+function add(a,b){
+    return a + b
+}
+
+function divide(a,b){
+    return a / b
+}
+
+function multiply(a,b){
+    return a * b
+}
+
+function mod(a,b){
+    return a % b
+}
+   
+function exponent(a,b){
+    return Math.pow(a, b)
+}
+    
+function subt(a,b){
+    return a - b
+}
+
+// assert.strictEqual(add(1,2),3)
+// assert.strictEqual(multiply(1,2),2)
+// assert.strictEqual(divide(2,1),2)
+// assert.strictEqual(mod(1,2),1)
+// assert.strictEqual(exponent(1,2),1)
+// assert.strictEqual(subt(1,2),-1)
