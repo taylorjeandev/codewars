b0b3b949db06edbcb4838676b40f9bd5a45b0b12
Taylor Jean <taylorgjean@gmail.com>
2022-08-17 19:45:10 +0200

    aug 17th

---
+++ get-ascii-value-of-character.js
@@ -0,0 +1,11 @@
+// Get ASCII value of a character.
+
+// For the ASCII table you can refer to http://www.asciitable.com/
+
+function getASCII(c){
+    return c.charCodeAt(0);
+}
+
+// Test.assertEquals(getASCII('A'),65)
+// Test.assertEquals(getASCII(' '),32)
+// Test.assertEquals(getASCII('!'),33)
