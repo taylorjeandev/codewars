ff49b64613fc5bd7708a151ed34aecf6105ee7e3
Taylor Jean <taylorgjean@gmail.com>
2022-09-01 21:44:33 +0200

    sept 1st

--- returning-strings.js
+++ returning-strings.js
@@ -3,7 +3,7 @@
 // [Make sure you type the exact thing I wrote or the program may not execute properly]
 
 function greet(name){
-    //your code here
+    return `Hello, ${name} how are you doing today?`
 }
 
 // assert.strictEqual(greet("Ryan"), "Hello, Ryan how are you doing today?");
