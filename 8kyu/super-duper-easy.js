f5383f2626784a452881b503a2d4f7abd8a8e71b
taylorjeandev <taylorgjean@gmail.com>
2023-04-10 19:58:35 +0200

    april 10 code challenges

---
+++ 8kyu/super-duper-easy.js
@@ -0,0 +1,16 @@
+// Make a function that returns the value multiplied by 50 and increased by 6. If the value entered is a string it should return "Error".
+
+function problem(x) {
+    return typeof x === 'string' ? "Error" : x * 50 + 6
+}
+
+console.log(problem("hello"), "Error");
+console.log(problem(1), 56);
+console.log(problem(5), 256);
+console.log(problem(0), 6);
+console.log(problem(1.2), 66);
+console.log(problem(3), 156);
+console.log(problem("RyanIsCool"), "Error");
+console.log(problem(-3), -144);
+console.log(problem(""), "Error");
+console.log(problem(0.03), 7.5);
