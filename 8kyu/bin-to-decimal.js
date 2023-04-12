2970c569ff4c8c66789b15d5d1987431f3f7cf8f
taylorjeandev <taylorgjean@gmail.com>
2023-04-12 23:12:51 +0200

    april 12th daily problem

---
+++ 8kyu/bin-to-decimal.js
@@ -0,0 +1,8 @@
+// Complete the function which converts a binary number (given as a string) to a decimal number.
+
+function binToDec(bin) {
+    return parseInt(bin, 2);
+}
+
+console.log(binToDec(1));
+console.log(binToDec(0));
