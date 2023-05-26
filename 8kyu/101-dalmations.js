c6ef0ff2a42d08e02395a4595a47c66ef2a48724
taylorjeandev <taylorgjean@gmail.com>
2023-05-27 01:01:11 +0200

    may 27th

--- 8kyu/101-dalmations.js
+++ 8kyu/101-dalmations.js
@@ -8,7 +8,7 @@
 
 function howManyDalmatians(number) {
 
-    var dogs = ["Hardly any", "More than a handful!", "Woah that's a lot of dogs!", "101 DALMATIONS!!!"];
+    var dogs = ["Hardly any", "More than a handful!", "Woah that's a lot of dogs!", "101 DALMATIANS!!!"];
 
     var respond = number <= 10 ? dogs[0] : number <= 50 ? dogs[1] : number === 101 ? dogs[3] : dogs[2];
 
