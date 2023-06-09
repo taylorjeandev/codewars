8f253e1a80a9ce80d46a8516199fc9b8edbf1e8a
taylorjeandev <taylorgjean@gmail.com>
2023-06-10 00:00:21 +0200

    july 10 daily problem

--- 7kyu/jaden-casing-strings.js
+++ 7kyu/jaden-casing-strings.js
@@ -8,8 +8,18 @@
 // Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"
 
 String.prototype.toJadenCase = function () {
-    //...
+    let arrayString = this.split(" ").map((word) => {
+        return word.charAt(0).toUpperCase() + word.slice(1);
+    }).join(" ");
+
+    return arrayString;
 };
+
+String.prototype.toJadenCase = function () {
+    return this.split(" ").map(function (word) {
+        return word.charAt(0).toUpperCase() + word.slice(1);
+    }).join(" ");
+}
 
 var str = "How can mirrors be real if our eyes aren't real";
 console.log(str.toJadenCase(), "How Can Mirrors Be Real If Our Eyes Aren't Real");
