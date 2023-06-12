c03dfd08bb1e79a3581dbccb67db87faf43216c7
taylorjeandev <taylorgjean@gmail.com>
2023-06-12 23:27:24 +0200

    june 12th daily prolems

---
+++ 7kyu/binary-addition.js
@@ -0,0 +1,25 @@
+// Implement a function that adds two numbers together and returns their sum in binary. The conversion can be done before, or after the addition.
+
+// The binary number returned should be a string.
+
+// Examples:(Input1, Input2 --> Output (explanation)))
+
+// 1, 1 --> "10" (1 + 1 = 2 in decimal or 10 in binary)
+// 5, 9 --> "1110" (5 + 9 = 14 in decimal or 1110 in binary)
+// BINARYFUNDAMENTALS
+// Ad
+// How to Create a Software Engineer Portfolio to Land Your First Junior Dev Job
+// A portfolio isn't optional. It's necessary. Find out how to create a standout portfolio. 
+// powered by 
+// Solution
+// 1
+// function addBinary(a,b) {
+// 2
+// ​
+// 3
+// }
+
+function addBinary(a, b) {
+    return (a + b).toString(2);
+
+    console.log(addBinary(1, 2), "11");
