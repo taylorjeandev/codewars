daa74c065b0b2f7a3eab0ec3a608729669d76f74
taylorjeandev <taylorgjean@gmail.com>
2024-03-07 21:21:24 +0100

    daily coding challenge

--- 8kyu/grasshopper-summation.js
+++ 8kyu/grasshopper-summation.js
@@ -1,7 +1,12 @@
 var summation = function (num) {
-
+    return num * (num + 1) / 2
 }
 
 console.log(summation(1), 1);
 console.log(summation(2), 3);
 console.log(summation(8), 36);
+
+// 1 * 1 = 1
+// 2 + 1 = 3
+// 3 + 2 + 1 = 6
+// 4 + 3 + 2 + 1 = 10
